2870165ff36851c73a384781608f0575e1cc75d6
Sergeev O.V <o.v.sergeyev@gmail.com>
2019-05-14 00:35:53 +0300

    Проект перенастроен на препроцессор sass

--- gulp/config/tasks.js
+++ gulp/config/tasks.js
@@ -3,8 +3,8 @@ module.exports = [
  './gulp/tasks/html-lint',
  './gulp/tasks/serve',
  //'./gulp/tasks/stylus',
- //'./gulp/tasks/sass',
- './gulp/tasks/less',
+ './gulp/tasks/sass',
+ //'./gulp/tasks/less',
  './gulp/tasks/watch',
  './gulp/tasks/img'
 ];
--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     $.gulp.task('less', function () {
-        return $.gulp.src('src/less/main.less')
+        return $.gulp.src('src/static/less/main.less')
             .pipe($.plumber({
-                    errorHandler: $.gp.notify.onError()
+                errorHandler: $.gp.notify.onError()
             }))
             //.pipe($.gp.sourcemaps.init())
             .pipe($.gp.less({}))
